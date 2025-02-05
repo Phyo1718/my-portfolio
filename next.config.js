b86bfb44cc50e314033161fe9dc2cb58d3ddc4e9
Phyo Wai Lin <phyowailin1718@gmail.com>
2025-02-05 19:58:03 +0700

    Made images unoptimized

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/my-portfolio',
+  images: {
+    unoptimized: true,  // Add this line
+  },
 }
 
 module.exports = nextConfig
